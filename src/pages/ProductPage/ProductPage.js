cfacba792720ea381e5374d1ecb0b5f4a623eb84
pumk <dangkhoa.duong@nfq.asia>
2022-06-08 10:38:24 +0700

    feat: add layout for list product

---
+++ src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ProductPage.scss';
+
+const ProductPage = () => {
+  return (
+    <div className='product-page-container'>
+      <div className='product-page-container__filter-container'></div>
+      <div className='product-page-container__products-list-view-container'></div>
+    </div>
+  );
+};
+
+export default ProductPage;
