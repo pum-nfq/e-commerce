54264489eda766c7d0f06b28cd4b2abd9c226a4f
pumk <dangkhoa.duong@nfq.asia>
2022-06-08 14:59:39 +0700

    style(layout, filter): add reponsive less than 768px

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Filter from '../../components/Filter/Filter';
 import './ProductPage.scss';
 
 const ProductPage = () => {
+  const onCheckFilter = (e) => {
+    console.log(e.target.checked);
+  };
+
   return (
-    <div className='product-page-container'>
-      <div className='product-page-container__filter-container'></div>
-      <div className='product-page-container__products-list-view-container'></div>
+    <div className="product-page-container">
+      <div className="product-page-container__filter-container">
+        <Filter onCheck={onCheckFilter} />
+      </div>
+      <div className="product-page-container__products-list-view-container"></div>
     </div>
   );
 };
