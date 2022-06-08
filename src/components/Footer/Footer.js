37311104f184123cf05f602e9be5a5a2c2a0ee01
pumk <dangkhoa.duong@nfq.asia>
2022-06-08 17:43:37 +0700

    feat: add footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import { SendOutlined } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
+import React from 'react';
+import './Footer.scss';
+import FooterContent from './FooterContent';
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-wrapper">
+      <div className="footer-wrapper__content-show">
+        <FooterContent label="THÔNG TIN LIÊN HỆ">
+          <b>Địa chỉ: Phường Cái Khế, Quận Ninh Kiểu, Thành phố Cần Thơ</b>
+          <b>Hotline: 0833495422</b>
+          <b>Email: akatsuki@nfq.asia</b>
+        </FooterContent>
 
-export default Footer
+        <FooterContent label="GIỜ LÀM VIỆC">
+          <b>9:30 đến 21:30</b>
+        </FooterContent>
+
+        <FooterContent label="HỖ TRỢ KHÁCH HÀNG">
+          <b>0833495422</b>
+        </FooterContent>
+
+        <FooterContent label="Đăng ký để nhận thông tin mới nhất để nhận khuyến mãi sớm nhất.">
+          <Input
+            suffix={
+              <Button type="text" icon={<SendOutlined className="icon" />} />
+            }
+            placeholder="example@email.com"
+            bordered={false}
+            className="footer-wrapper__content-show__input"
+          />
+        </FooterContent>
+      </div>
+      <p className="footer-wrapper__copy-right">
+        ALL RIGHTS RESERVED. WEB DESIGN : NFQ CO., LTD
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
