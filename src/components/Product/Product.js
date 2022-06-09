0d117e70828daf3da931037f5d8119ff882d2296
DucTranNFQ <duc.tran@nfq.asia>
2022-06-09 12:32:15 +0700

    chore: text overflow ellipsis

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,24 +6,24 @@ import "antd/dist/antd.css";
 
 export default function Product({imgSource, linkToDetail, productBrand, productName, productPrice}) {
     return (
-        <div className="Product-card">
-            <div style={{ width: "100%", overflow: "hidden" }}>
-                <a href={linkToDetail} className="Product-card__link">
+        <div className="product-card">
+            <div style={{ overflow: "hidden" }}>
+                <a href={linkToDetail} className="product-card__link">
                     <img
-                        className="Product-card__img"
+                        className="product-card__img"
                         src={imgSource}
                     />
                 </a>
             </div>
-            <h4 className="Product-card__title">{productBrand}</h4>
-            <a href={linkToDetail} className="Product-card__link">
-                <p className="Product-card__text">
+            <h4 className="product-card__title">{productBrand}</h4>
+            <a href={linkToDetail} className="product-card__link">
+                <p className="product-card__text">
                     {productName}
                 </p>
             </a>
-            <p className="Product-card__price">{productPrice}</p>
+            <p className="product-card__price">{productPrice}</p>
             <Button
-                className="Product-card__button"
+                className="product-card__button"
                 block
                 type="text"
                 icon={<PlusOutlined />}
