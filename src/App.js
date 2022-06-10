afab7d51206e11a71df0b440aa79d6f54fb30e31
DucTranNFQ <duc.tran@nfq.asia>
2022-06-10 17:18:19 +0700

    combine navbar, product-page, footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import "antd/dist/antd.min.css";
 import "./general.scss";
 
-import MainLayout from "./Layouts/MainLayout/MainLayout";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import ProductPage from "./pages/ProductPage/ProductPage"
 
 function App() {
     return (
@@ -12,11 +14,15 @@ function App() {
                 <Route
                     path="/"
                     element={
-                        <MainLayout />
+                        <>
+                            <Navbar />
+                            <Outlet />
+                            <Footer />
+                        </>
                     }
                 >
-                    <Route index element={"home"} />
-                    <Route path="product" element={"product-page"} />
+                    <Route index element={<div>home</div>} />
+                    <Route path="product" element={<ProductPage />} />
                     <Route path="admin" element={"admin"} />
                     <Route path="detail/*" element={"detail"} />
                 </Route>
