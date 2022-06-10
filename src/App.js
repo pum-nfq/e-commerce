33b8617f5be0d8bf11e3b334925b6ca477386faa
DucTranNFQ <duc.tran@nfq.asia>
2022-06-10 13:58:51 +0700

    Install react-router-dom@6 and build app routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import 'antd/dist/antd.min.css'
-import './general.scss';
-import ProductPage from './pages/ProductPage/ProductPage';
+import { Routes, Route, Outlet } from "react-router-dom";
+import "antd/dist/antd.min.css";
+import "./general.scss";
+
+import MainLayout from "./Layouts/MainLayout/MainLayout";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
-  return (
-    <div className="app">
-      <ProductPage />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <MainLayout />
+                    }
+                >
+                    <Route index element={"home"} />
+                    <Route path="product" element={"product-page"} />
+                    <Route path="admin" element={"admin"} />
+                    <Route path="detail/*" element={"detail"} />
+                </Route>
+
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
