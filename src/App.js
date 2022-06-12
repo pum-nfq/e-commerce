13665882cfb6ede0b7546772dd58eece9c821794
pumk <dangkhoa.duong@nfq.asia>
2022-06-12 14:40:21 +0700

    change import lib stylesheet to index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { Routes, Route, Outlet } from "react-router-dom";
-import "antd/dist/antd.min.css";
-import "./general.scss";
-
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
@@ -9,29 +6,29 @@ import ProductPage from "./pages/ProductPage/ProductPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
 
 function App() {
-    return (
-        <div className="app">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <Navbar />
-                            <Outlet />
-                            <Footer />
-                        </>
-                    }
-                >
-                    <Route index element={<div>home</div>} />
-                    <Route path="product" element={<ProductPage />} />
-                    <Route path="admin" element={<AdminPage />} />
-                    <Route path="detail/*" element={"detail"} />
-                </Route>
+  return (
+    <div className="app">
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <Navbar />
+                        <Outlet />
+                        <Footer />
+                    </>
+                }
+            >
+                <Route index element={<div>home</div>} />
+                <Route path="product" element={<ProductPage />} />
+                <Route path="admin" element={<AdminPage />} />
+                <Route path="detail/*" element={"detail"} />
+            </Route>
 
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </div>
-    );
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
