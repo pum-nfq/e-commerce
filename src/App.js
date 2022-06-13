702eb58f2fb125daad8ac1b3d3baaf6cdb2e2833
pumk <dangkhoa.duong@nfq.asia>
2022-06-13 16:37:45 +0700

    feat: completed admin page CRUD product

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-import "antd/dist/antd.min.css";
-import "./general.scss";
+import { Routes, Route, Outlet } from 'react-router-dom';
+import 'antd/dist/antd.min.css';
+import './general.scss';
 
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
-import ProductPage from "./pages/ProductPage/ProductPage";
-import AdminPage from "./pages/AdminPage/AdminPage";
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+import ProductPage from './pages/ProductPage/ProductPage';
+import AdminPage from './pages/AdminPage/AdminPage';
 
 function App() {
-    return (
-        <div className="app">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <Navbar />
-                            <Outlet />
-                            <Footer />
-                        </>
-                    }
-                >
-                    <Route index element={<div>home</div>} />
-                    <Route path="product" element={<ProductPage />} />
-                    <Route path="admin" element={<AdminPage />} />
-                    <Route path="detail/*" element={"detail"} />
-                </Route>
-
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Outlet />
+              <Footer />
+            </>
+          }
+        >
+          <Route index element={<div>home</div>} />
+          <Route path="product" element={<ProductPage />} />
+          <Route path="detail/*" element={'detail'} />
+        </Route>
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
