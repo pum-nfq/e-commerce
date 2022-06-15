810e4b54a79ba0cb4632a692d8d73ce265721040
pumk <dangkhoa.duong@nfq.asia>
2022-06-15 13:19:01 +0700

    fix: resolved id product undefined when push to url

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -25,7 +25,7 @@ export default function ProductList({ title, data }) {
         }}
         renderItem={(item) => (
           <List.Item>
-            <Product {...item} price={item.sizes[0].price} />
+            <Product {...item} price={item.sizes[0].price} id={item.key} />
           </List.Item>
         )}
       />
