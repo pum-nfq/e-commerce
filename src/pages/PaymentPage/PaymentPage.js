04b5fc4cbcc2462525c1f6d224c5d75ce3eb3c1f
duyvodev <duy.vo@nfq.asia>
2022-06-16 17:35:06 +0700

    fix: add cart payment layout

--- src/pages/PaymentPage/PaymentPage.js
+++ src/pages/PaymentPage/PaymentPage.js
@@ -1,17 +1,94 @@
-import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Layout } from "antd";
-import { Content } from "antd/lib/layout/layout";
-import { Link } from "react-router-dom";
-import "./PaymentPage.scss";
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Breadcrumb, Button, Layout } from 'antd';
+import { Typography } from 'antd';
+import { Table } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+import './PaymentPage.scss';
+
+const { Title } = Typography;
+
+const columns = [
+  {
+    title: 'Image',
+    dataIndex: 'image',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+  },
+  {
+    title: 'Quantity',
+    dataIndex: 'quantity',
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+  },
+];
+const data = [
+  //   {
+  //     key: '1',
+  //     name: 'John Brown',
+  //     age: 32,
+  //     address: 'New York No. 1 Lake Park',
+  //   },
+  //   {
+  //     key: '2',
+  //     name: 'Jim Green',
+  //     age: 42,
+  //     address: 'London No. 1 Lake Park',
+  //   },
+  //   {
+  //     key: '3',
+  //     name: 'Joe Black',
+  //     age: 32,
+  //     address: 'Sidney No. 1 Lake Park',
+  //   },
+  {
+    key: '1',
+    image: 'src',
+    name: 'test',
+    price: 120,
+    quantity: 1,
+    amount: 2,
+  },
+  {
+    key: '2',
+    image: 'src',
+    name: 'test',
+    price: 120,
+    quantity: 1,
+    amount: 2,
+  },
+];
 
 export default function PaymentPage() {
+  // const data = useRef(() => {
+  //   return [
+  //     {
+  //       key: '1',
+  //       image: 'src',
+  //       name: 'test',
+  //       price: 120,
+  //       quantity: 1,
+  //       amount: 2,
+  //     },
+  //   ];
+  // });
   return (
     <div className="cart__wrapper">
       <Layout>
         <Content className="site-layout">
           <Breadcrumb
             style={{
-              margin: "16px 0",
+              margin: '16px 0',
             }}
           >
             <Breadcrumb.Item>
@@ -21,7 +98,7 @@ export default function PaymentPage() {
           </Breadcrumb>
           <div className="cart__content-wrapper">
             <div className="cart__content">
-              <div className="cart__content-icon">
+              {/* <div className="cart__content-icon">
                 <ShoppingCartOutlined />
               </div>
               <div className="cart__content-description">
@@ -29,7 +106,12 @@ export default function PaymentPage() {
               </div>
               <Button>
                 <Link to="/">Back to home</Link>
-              </Button>
+              </Button> */}
+              <div className="cart__cartInfo">
+                <Title level={4}>your cart</Title>
+                <Table columns={columns} dataSource={data} size="middle" />
+              </div>
+              <div className="cart__deliverInfo"></div>
             </div>
           </div>
         </Content>
