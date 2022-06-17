8289a2570eee52745be7302fff4335e798c27453
DucTranNFQ <duc.tran@nfq.asia>
2022-06-17 15:59:09 +0700

    Add HomePage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar';
 import AdminPage from './pages/AdminPage/AdminPage';
 import DetailProductPage from './pages/DetailProductPage/DetailProductPage';
+import HomePage from './pages/HomePage/HomePage';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import ProductPage from './pages/ProductPage/ProductPage';
 
@@ -21,7 +22,7 @@ function App() {
             </>
           }
         >
-          <Route index element={<div>home</div>} />
+          <Route index element={<HomePage />} />
           <Route path="product" element={<ProductPage />} />
           <Route path="admin" element={<AdminPage />} />
           <Route path="detail/:id" element={<DetailProductPage />} />
