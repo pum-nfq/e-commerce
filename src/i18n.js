52055b9221ba069372747d89f410939fa367bcf8
pumk <dangkhoa.duong@nfq.asia>
2022-06-17 12:03:01 +0700

    install react-i18next

---
+++ src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './translations/en.json';
+import vi from './translations/vi.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  vi: {
+    translation: vi,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
